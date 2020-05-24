9a3f63a026e6dd7c98435179acb7c2514e5b1a59
ngel rojas p <aomarrojasp@gmail.com>
2020-05-24 16:10:46 -0300

    add repository queries

---
+++ app/core/queries/HelperCompany.py
@@ -0,0 +1,12 @@
+from core.profile import CompanyProfile
+
+
+class HelperCompany:
+    def getAllCompanies(self, request):
+        result = CompanyProfile.objects.filter(user=request.user)
+        return result
+
+    def deleteCompany(self, request, pk):
+        result = CompanyProfile.objects.get(id=pk, user=request.user)
+        result.delete()
+        return True
