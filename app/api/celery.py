735d5e862c4a7b8e280db3e19d94a74b7e62c055
ngel rojas <aomarrojasp@gmail.com>
2020-08-01 21:01:16 -0300

    chante to production

--- app/api/celery.py
+++ app/api/celery.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from celery import Celery, shared_task
 from core.email import CotizateSendEmail
-from api.settings import development
+from api.settings import production
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.development')
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.production")
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.development')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.production")
 app = Celery("api", broker="pyamqp://rabbitmq_user:admin2020@broker//")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
@@ -18,7 +18,7 @@ app.autodiscover_tasks()
 def send_email_module(self, subject, to, body, template_name=None, context=None):
     try:
         CotizateSendEmail(
-            subject=subject, to=to, from_email=development.DEFAULT_FROM_EMAIL, body=body,
+            subject=subject, to=to, from_email=production.DEFAULT_FROM_EMAIL, body=body,
         ).send_email_with_custom_template(
             template_name=template_name, context=context,
         )
