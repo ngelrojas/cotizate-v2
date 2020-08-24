947dce717c743926b511ece54182f650aa80b9b6
ngel rojas <aomarrojasp@gmail.com>
2020-08-24 14:55:37 -0300

    add campaings status for each one them

--- app/campaings/public.py
+++ app/campaings/public.py
@@ -1,14 +1,34 @@
 from rest_framework import viewsets
 from rest_framework import filters
 from core.campaing import Campaing
-from .serializers import CampaingSerializer
+from .serializers import CampaingPublicSerializer
 
 
-class CampaingPublicView(viewsets.ModelViewSet):
+class CampaingPublic(viewsets.ModelViewSet):
     """
     show all campaing public
     """
-    serializer_class = CampaingSerializer
+    serializer_class = CampaingPublicSerializer
     queryset = Campaing.get_all_available()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', ]
+
+
+class CampaingCompleted(viewsets.ModelViewSet):
+    """
+    show all campaing public completed
+    """
+    serializer_class = CampaingPublicSerializer
+    queryset = Campaing.get_completed_campaings()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', ]
+
+
+class CampaingTerminated(viewsets.ModelViewSet):
+    """
+    show all campaing public terminated
+    """
+    serializer_class = CampaingPublicSerializer
+    queryset = Campaing.get_terminated_campaings()
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', ]
