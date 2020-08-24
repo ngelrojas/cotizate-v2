1df8c94d37a38805ba189316163ca32f5ac93281
ngel rojas <aomarrojasp@gmail.com>
2020-08-24 14:56:17 -0300

    add new urls

--- app/campaings/urls.py
+++ app/campaings/urls.py
@@ -1,20 +1,25 @@
 from django.urls import path
 from .views import Campaings
-# from .public import CampaingPublicView
+from .public import CampaingPublic, CampaingCompleted, CampaingTerminated
 
 app_name = 'campaing-app'
-# TODO: complete campaing HERE
+
 urlpatterns = [
     path('campaing', Campaings.as_view({
         'get': 'list',
         'post': 'create'}), name='campaing'),
-    # path('campaingsl', Campaings.as_view(), name='campaing'),
 
-    # path('campaing/<int:pk>', CampaingView.as_view({
-    #     'get': 'retrieve',
-    #     'put': 'update',
-    #     'delete': 'delete',
-    #     'patch': 'patch'}), name='detail-campaing'),
-    # path('campaings', CampaingPublicView.as_view({
-    #     'get': 'list'}), name='public-campaing')
+    path('campaing/<int:pk>', Campaings.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'delete'}), name='detail-campaing'),
+
+    path('campaing-public', CampaingPublic.as_view({
+        'get': 'list'}), name='public-campaing'),
+
+    path('campaing-completed', CampaingCompleted.as_view({
+        'get': 'list'}), name='complete-campaing'),
+
+    path('campaing-terminated', CampaingTerminated.as_view({
+        'get': 'list'}), name='terminated-campaing'),
 ]
