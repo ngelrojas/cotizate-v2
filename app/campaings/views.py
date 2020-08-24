98a56c9c007d96c375de4e5d3a0b6fa756eb77d9
ngel rojas <aomarrojasp@gmail.com>
2020-08-24 14:56:48 -0300

    add new views

--- app/campaings/views.py
+++ app/campaings/views.py
@@ -2,11 +2,8 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
 from core.campaing import Campaing
-from .serializers import CampaingSerializer
-# from .serializers import CCampaingSerializer
-# from api.cotizate import ActiveUser, ProfileComplete
 from core.profile import PersonalProfile
-# from core.profile import CompanyProfile
+from .serializers import CampaingSerializer
 
 
 class Campaings(viewsets.ViewSet):
@@ -22,30 +19,74 @@ class Campaings(viewsets.ViewSet):
     def list(self, request):
         """list all campaings to current user"""
         try:
-            list_campaing = Campaing.objects.filter(users=request.user)
+            list_campaing = Campaing.get_not_del_and_archived(self, request)
             serializer = self.serializer_class(list_campaing, many=True)
             return Response({'data': serializer.data}, status=status.HTTP_200_OK)
         except Exception as err:
             return Response({'error': f'{err}'},
                             status=status.HTTP_400_BAD_REQUEST)
 
-    # TODO: complete Campaing HERE
     def create(self, request):
         """create campaing to current user"""
         try:
-            # get current profile
             data = request.data.copy()
             current_profile = PersonalProfile.objects.get(user=request.user)
             if current_profile.complete is not True:
-                return Response({'data': 'profile user is not complete'}, status=status.HTTP_200_OK)
+                return Response({'data': False,
+                                 'msg': 'profile user is not complete'},
+                                status=status.HTTP_200_OK)
 
             data['users'] = request.user.id
             data['profiles'] = request.user.id
             serializer = self.serializer_class(data=data)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
-                return Response({'data': 'campaing is saved.'}, status=status.HTTP_201_CREATED)
+                return Response({'data': True,
+                                 'msg': 'campaing is saved.'},
+                                status=status.HTTP_201_CREATED)
 
         except Exception as err:
-            return Response({'error': f'{err}'},
+            return Response({'data': False,
+                             'mgs': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+        """retrieve campaing to current user and ID campaing"""
+        try:
+            current_campaing = Campaing.retrieve_campaing(self, request.user, pk)
+            serializer = self.serializer_class(current_campaing)
+            return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+        """update current campaing to current user and ID campaing"""
+        try:
+            current_campaing = Campaing.retrieve_campaing(self, request.user, pk)
+            serializer = self.serializer_class(
+                current_campaing,
+                data=request.data,
+                partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({'data': True,
+                                 'msg': 'campaing updated.'},
+                                status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """retrieve campaing to current user and ID campaing"""
+        try:
+            Campaing.update_to_delete(self, request.user, pk)
+            return Response({'data': True,
+                             'msg': 'campaing deleted.'},
+                            status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
                             status=status.HTTP_400_BAD_REQUEST)
