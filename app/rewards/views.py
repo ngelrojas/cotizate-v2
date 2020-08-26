ac7aeb11d9788dba57a693e02c3b715e38775582
ngel rojas <aomarrojasp@gmail.com>
2020-08-25 21:12:41 -0300

    update function

--- app/rewards/views.py
+++ app/rewards/views.py
@@ -5,7 +5,7 @@ from .serializers import RewardSerializer
 from core.reward import Reward
 
 
-class RewardView(viewsets.ModelViewSet):
+class RewardView(viewsets.ViewSet):
     """
         list:
             - list all rewards
@@ -21,18 +21,20 @@ class RewardView(viewsets.ModelViewSet):
         try:
             queryset = Reward.get_all_reward(
                 self,
+                request,
                 request.data.get('campaing_id'))
             serializer = self.serializer_class(queryset, many=True)
             return Response({'data': serializer.data},
                             status=status.HTTP_200_OK)
         except Reward.DoesNotExist as err:
-            return Response({'error': f'{err}'},
+            return Response({'data': False,
+                             'msg': f'{err}'},
                             status=status.HTTP_404_NOT_FOUND)
 
     def retrieve(self, request, pk=None):
         """retrieve reward current campaing"""
         try:
-            current_reward = Reward.get_reward(self, pk)
+            current_reward = Reward.get_reward(self, request, pk)
             serializer = self.serializer_class(current_reward)
             return Response({'data': serializer.data},
                             status=status.HTTP_200_OK)
@@ -40,35 +42,52 @@ class RewardView(viewsets.ModelViewSet):
             return Response({'error': f'{err}'},
                             status=status.HTTP_404_NOT_FOUND)
 
-    def perform_create(self, serializer):
+    def create(self, request):
         """create reward"""
-        return serializer.save()
+        try:
+            data = request.data.copy()
+            data['users'] = request.user.id
+            serializer = self.serializer_class(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({'data': True,
+                                 'msg': 'reward saved.'},
+                                status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return serializer.save(owner=self.request.user)
 
-    def update(self, request, pk=None):
+    def update(self, request, pk):
         """update reward"""
         try:
-            current_reward = Reward.get_reward(self, pk)
+            current_reward = Reward.get_reward(self, request, pk)
             data_send = {
                 'title': request.data.get('title'),
                 'description': request.data.get('description'),
                 'amount': request.data.get('amount'),
                 'campaings': current_reward.id,
-                'users': request.data.get('users')}
+                'users': request.user.id}
             serializer = self.serializer_class(current_reward, data=data_send)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
-                return Response({'data': serializer.data},
+                return Response({'data': serializer.data,
+                                 'msg': 'reward updated.'},
                                 status=status.HTTP_200_OK)
         except Reward.DoesNotExist as err:
-            return Response({'error': f'{err}'},
+            return Response({'data': False,
+                             'msg': f'{err}'},
                             status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self, request, pk=None):
+    def delete(self, request, pk):
         """delete reward"""
         try:
-            Reward.delete_reward(self, pk)
-            return Response({'data': 'reward deleted'},
+            Reward.delete_reward(self, request, pk)
+            return Response({'data': True,
+                             'msg': 'reward deleted.'},
                             status=status.HTTP_204_NO_CONTENT)
         except Reward.DoesNotExist as err:
-            return Response({'error': f'{err}'},
+            return Response({'data': False,
+                             'msg': f'{err}'},
                             status=status.HTTP_404_NOT_FOUND)
