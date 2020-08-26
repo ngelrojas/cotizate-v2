f4a37920438862cb0826f040a4aebaf39fb28f83
ngel rojas <aomarrojasp@gmail.com>
2020-08-25 21:11:30 -0300

    update reward queries

--- app/core/reward.py
+++ app/core/reward.py
@@ -12,11 +12,29 @@ class Reward(models.Model):
     def __str__(self):
         return self.title
 
-    def get_all_reward(self, campaingId):
-        return Reward.objects.filter(campaings=campaingId)
+    def get_all_reward(self, request, campaingId):
+        """private function"""
+        return Reward.objects.filter(
+            users=request.user.id,
+            campaings=campaingId)
 
-    def get_reward(self, rewardId):
-        return Reward.objects.get(id=rewardId)
+    def get_reward(self, rewardId, request):
+        """private function"""
+        return Reward.objects.get(users=request.user.id, id=rewardId)
 
-    def delete_reward(self, rewardId):
-        return Reward.objects.get(id=rewardId).delete()
+    def delete_reward(self, request, rewardId):
+        """private function"""
+        return Reward.objects.get(
+            users=request.user.id,
+            id=rewardId).delete()
+
+    def delete_all_reward(self, campaingID, request):
+        return Reward.objects.filter(
+            users=request.user.id,
+            campaings=campaingID).delete()
+
+    def get_all_reward_public(self, campaingID):
+        """
+        public
+        retrieve all reward about the current campaing"""
+        return Reward.objects.filter(campaings=campaingID)
