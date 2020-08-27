cf154910e3b44f458dbf3c4d65f1074f0cfe6194
ngel rojas <aomarrojasp@gmail.com>
2020-08-26 23:38:42 -0300

    update functions

--- app/users/views.py
+++ app/users/views.py
@@ -17,7 +17,7 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
 
 
-class UpdateUserView(viewsets.ModelViewSet):
+class UpdateUserView(viewsets.ViewSet):
     """update main data current user"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
@@ -29,23 +29,28 @@ class UpdateUserView(viewsets.ModelViewSet):
             serializer = self.serializer_class(current_user)
             return Response({'data': serializer.data},
                             status=status.HTTP_200_OK)
-        except User.DoesNotExist as err:
-            return Response({'error': f'{err}'},
-                            status=status.HTTP_404_NOT_FOUND)
+        except Exception as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, pk=None):
         """update data partial current user"""
-        current_user = ActiveUser.currentUser(self, request)
-        serializer = self.serializer_class(
-            current_user,
-            data=request.data,
-            partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response({'data': 'user updated'},
-                            status=status.HTTP_200_OK)
-        return Response({'error': 'something wrong happend'},
-                        status=status.HTTP_400_BAD_REQUEST)
+        try:
+            current_user = ActiveUser.currentUser(self, request)
+            serializer = self.serializer_class(
+                current_user,
+                data=request.data,
+                partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({'data': True,
+                                 'msg': 'user updated'},
+                                status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
         """update state current user to deleted"""
@@ -54,11 +59,13 @@ class UpdateUserView(viewsets.ModelViewSet):
             current_user.deleted = True
             current_user.is_activate = False
             current_user.save()
-            return Response({'data': 'user deleted'},
+            return Response({'data': True,
+                             'msg': 'user deleted'},
                             status=status.HTTP_204_NO_CONTENT)
         except User.DoesNotExist as err:
-            return Response({'error': f'{err}'},
-                            status=status.HTTP_404_NOT_FOUND)
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class RecoveryPasswordUser(viewsets.ModelViewSet):
@@ -86,13 +93,18 @@ class RecoveryPwdConfirm(viewsets.ModelViewSet):
                 if serializer.is_valid(raise_exception=True):
                     current_user.set_password(request.data.get('password'))
                     current_user.save()
-                    return Response({'data': 'password updated.'},
+                    return Response({'data': True,
+                                     'msg': 'password updated.'},
                                     status=status.HTTP_200_OK)
 
-                return Response({'error': 'user is not activated.'},
-                                status=status.HTTP_200_OK)
-            return Response({'error': 'some of data is required'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        except User.DoesNotExist as err:
-            return Response({'error': f'{err}'},
+                return Response({'data': False,
+                                 'msg': 'user is not activated.'},
+                                status=status.HTTP_404_NOT_FOUND)
+
+            return Response({'data': False,
+                             'msg': 'some of data is required'},
                             status=status.HTTP_404_NOT_FOUND)
+        except User.DoesNotExist as err:
+            return Response({'data': False,
+                             'msg': f'{err}'},
+                            status=status.HTTP_400_BAD_REQUEST)
