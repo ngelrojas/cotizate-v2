d022793fb3e0295afcdd0ac3b79acaa553511fb3
ngel rojas <aomarrojasp@gmail.com>
2020-11-21 23:37:53 -0300

    add new methods

--- app/core/queries/rewardQuery.py
+++ app/core/queries/rewardQuery.py
@@ -15,3 +15,26 @@ class RewardQuery:
     def retrieve_reward(pk, header_id):
         """retrieve reward campaing header"""
         return Reward.objects.get(id=pk, header=header_id)
+
+    @classmethod
+    def saving_rewards(cls, request):
+        """
+        saving rewards
+        header"""
+        try:
+            header_id = CampaingHeader.objects.get(id=request.data.get("header"))
+            rewards = Reward.objects.create(
+                title=request.data.get("title"),
+                description=request.data.get("description"),
+                amount=request.data.get("amount"),
+                expected_delivery=request.data.get("expected_delivery"),
+                header=header_id,
+                all_cities=request.data.get("all_cities"),
+                pick_up_locally=request.data.get("pick_up_locally"),
+            )
+            for city in request.data.get("cities"):
+                rewards.cities.add(city)
+
+            return True
+        except Exception:
+            return False
