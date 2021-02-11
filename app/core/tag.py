52afc4905f218ae5ab74ccc5fddd6f5d824762da
p1200100 <p1200100@brasilprev.com.br>
2021-02-10 23:00:45 -0300

    add tags

--- app/core/tag.py
+++ app/core/tag.py
@@ -1,6 +1,7 @@
+from django.db import models
 from .abstract import AbstractItem
-
+from .campaing import CampaingHeader
 
 class Tag(AbstractItem):
     """category tag"""
-    pass
+    campaings = models.ForeignKey(CampaingHeader, on_delete=models.CASCADE)
--- app/tags/views.py
+++ app/tags/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-from rest_framework import filters
+from rest_framework import status
+from rest_framework.response import Response 
 from .serializers import TagSerializer
 from core.tag import Tag
 
@@ -13,5 +14,32 @@ class TagView(viewsets.ModelViewSet):
     """
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name', ]
+
+    def list(self, request, campId):
+        """
+            list tags about campaing
+        """
+        try:
+            list_tags = Tag.objects.filter(campaings=campId)
+            serializer = self.serializer_class(list_tags, many=True)
+
+            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+        except Exception as err:
+            return Response(
+                {"data": False, "msg": f"{err}"}, status=status.HTTP_404_NOT_FOUND
+            )
+    
+    def create(self, request):
+        """
+            create tag about campaing
+        """
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"data": "tag saved."}, status=status.HTTP_201_CREATED)
+        except Exception as err:
+            return Response(
+                {"data": False, "msg": f"{err}"}, status=status.HTTP_404_NOT_FOUND
+            )
