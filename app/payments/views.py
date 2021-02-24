9a1491bef321c96ac330387b543f05a1e931778d
p1200100 <p1200100@brasilprev.com.br>
2021-02-24 18:14:55 -0300

    fix errro

--- app/payments/views.py
+++ app/payments/views.py
@@ -60,11 +60,11 @@ class PaymentView(viewsets.ModelViewSet):
             lcDatosPago3 = f"|{lcParametro3}|{lcParametro4}"
 
             lcDatosPago = lcDatosPago1 + lcDatosPago2 + lcDatosPago3
-            # tcParametros = cryp3DES.encrypt_3des(self, lcDatosPago, lcTokenSecret)
-            tcParametros = "YzBoTnUrWGxCM1FCcTI3YjF2RFFrcmdaN1hpWG85bmxxU0wxQjgzVjBQMVlRMU5Ka01NVTE1b3lDamNCdmcvMXZJNHVxQXFUNEFRRDVZVVNDcmNSSkRDTGVIVkd5RGNSYVQ1c2t5bFhteWJuVmxMbUNlbUNFd0dQeW5GVTM2eUk5cjRMWTFwcisvMnBGenhUSDh1RWpjZ0wrZFEzWUxNZ3ZkL2tNY0JEcWd0RDBuTWoxMk1rQlR1STBZNTNZYnBpQ2JvSzRTNnJOTTgxYXVZWGJTWFVZL1N4S2YwckliSWV0b1ZKSU1jcnFETmxxZHlEOW9XaGE0a2lRRzZadm94RW9GYnJNaHdZdCtMMjJxQ2I0QlVrSUMvQkFJajROQ2JoNkdxWktXL21KcjRoc202WEdDN0YrUWZ4NE0xSzhjSjlZVFgvait5dEY2UEJhM0xHbGJBNlM4bEJQS0ZhaUFnOEc4QzU2bW8yN1ovanYvZytCZWQrcFgyeWltSmRxT0R5QTFHKzA3SVQyT1puRDU2NDAzZXVqL1Q5ems5WmNzV3hYK2c3VUl4bVJObUJzdUkyODF5N0NRPT0="
-            DtcParametros = cryp3DES.decrypt_3des(self, tcParametros, lcTokenSecret)
+            tcParametros = cryp3DES.encrypt_3des(self, lcDatosPago, lcTokenSecret)
+            # tcParametros = "YzBoTnUrWGxCM1FCcTI3YjF2RFFrcmdaN1hpWG85bmxxU0wxQjgzVjBQMVlRMU5Ka01NVTE1b3lDamNCdmcvMXZJNHVxQXFUNEFRRDVZVVNDcmNSSkRDTGVIVkd5RGNSYVQ1c2t5bFhteWJuVmxMbUNlbUNFd0dQeW5GVTM2eUk5cjRMWTFwcisvMnBGenhUSDh1RWpjZ0wrZFEzWUxNZ3ZkL2tNY0JEcWd0RDBuTWoxMk1rQlR1STBZNTNZYnBpQ2JvSzRTNnJOTTgxYXVZWGJTWFVZL1N4S2YwckliSWV0b1ZKSU1jcnFETmxxZHlEOW9XaGE0a2lRRzZadm94RW9GYnJNaHdZdCtMMjJxQ2I0QlVrSUMvQkFJajROQ2JoNkdxWktXL21KcjRoc202WEdDN0YrUWZ4NE0xSzhjSjlZVFgvait5dEY2UEJhM0xHbGJBNlM4bEJQS0ZhaUFnOEc4QzU2bW8yN1ovanYvZytCZWQrcFgyeWltSmRxT0R5QTFHKzA3SVQyT1puRDU2NDAzZXVqL1Q5ems5WmNzV3hYK2c3VUl4bVJObUJzdUkyODF5N0NRPT0="
+            # DtcParametros = cryp3DES.decrypt_3des(self, tcParametros, lcTokenSecret)
 
-            laData = {"tcParametros": DtcParametros, "tcCommerceID": tcCommerceID}
+            laData = {"tcParametros": tcParametros, "tcCommerceID": tcCommerceID}
 
             return Response({"data": laData}, status=status.HTTP_200_OK)
 
