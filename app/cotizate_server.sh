da6ee28ce11e65ea30b2f2c9d995f93079d48495
p1200100 <p1200100@brasilprev.com.br>
2021-04-10 18:28:51 -0300

    add celery on supervisor

--- app/cotizate_server.sh
+++ app/cotizate_server.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-gunicorn --bind 0.0.0.0:16610 api.wsgi:application
+# gunicorn --bind 0.0.0.0:16610 api.wsgi:application
+
+docker-compose run api sh -c "celery -A api.celery worker -l INFO"
