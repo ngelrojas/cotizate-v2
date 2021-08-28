f6f8545fdf257576fe33078aa77a609b3d8ecce3
aomarrojasp@gmail.com <aomarrojasp@gmail.com>
2021-08-27 22:37:20 -0300

    new server from

--- app/api/settings/production.py
+++ app/api/settings/production.py
@@ -15,7 +15,7 @@ import datetime
 from corsheaders.defaults import default_methods
 from corsheaders.defaults import default_headers
 from decouple import config
-from .conf_email import *
+# from .conf_email import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
